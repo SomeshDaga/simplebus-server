b6ed5f0920ac23ac67843991a7336d69a3bfa9e0
James Liu <jamesjhliu@gmail.com>
2014-07-25 15:19:49 -0700

    more port changes

--- start.py
+++ start.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 url = 'http://api.translink.ca/rttiapi/v1/'
 api = {'apikey':'Y0SgOFBTcRPW2nkOx9gA'}
 header = {'content-type':'application/JSON', 'accept':'application/JSON'}
+port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def indexhandler():
@@ -35,4 +36,4 @@ def estimate(stop_id):
 0
 
 if __name__ == '__main__':
-    app.run(port=33507)
+    app.run(host='0.0.0.0', port=port)
