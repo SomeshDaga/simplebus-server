036a9dcc4aadc8fa2c5fab040b8e44d78ab34ffa
James Liu <jamesjhliu@gmail.com>
2014-08-01 14:32:55 -0700

    add proper CORS header handling

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
 import os, requests
-from flask import Flask, request, session, g
+from flask import Flask, make_response, request, current_app
+from datetime import timedelta
+from functools import update_wrapper
 app = Flask(__name__)
+
+
 
 # app.config.update(dict(
 #     DEBUG = False,
@@ -14,15 +18,18 @@ header = {'content-type':'application/JSON', 'accept':'application/JSON'}
 port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
-def indexhandler():
+@crossdomain(origin='*')
+def index():
     return 'Hello world!'
 
 @app.route('/api/stop/<stop_id>')
+@crossdomain(origin='*')
 def stop(stop_id):
     r = requests.get(url + 'stops/' + stop_id, params = api, headers = header)
     return r.text
 
 @app.route('/api/stop/<stop_id>/<route_id>')
+@crossdomain(origin='*')
 def stop_route(stop_id, route_id):
     stop_route_api = api
     stop_route_api['route'] = route_id
@@ -30,10 +37,53 @@ def stop_route(stop_id, route_id):
     return r.text
 
 @app.route('/api/estimate/<stop_id>')
+@crossdomain(origin='*')
 def estimate(stop_id):
     r = requests.get(url + 'stops/' + stop_id + '/estimates', params = api, headers = header)
     return r.text
-0
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
